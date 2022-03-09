bd1df75572a78af395a570280be42faddc3e22c4
thomelect <77449802+thomelect@users.noreply.github.com>
2022-03-09 18:21:58 -0500

    ~ Modification du diviseur d'ADC afin d'être capable de lire 0 - 100°C. + Ajout d'une fonction qui fait la moyenne des valeurs dans un tableau afin de réduire les fluctuations. [FONCTIONNEL]

--- MOTRIK_capt-temperature/main.c
+++ MOTRIK_capt-temperature/main.c
@@ -19,12 +19,18 @@
 #include "adc.h"
 #include "usart.h"
 
+#define FILTRE_SIZE 5
+
 volatile uint8_t cntCinqCentMs = 0;
 volatile uint8_t cinqCentMSFlag = 0;
 volatile uint8_t refreshMesure = 0;
 char msgTemp[17];
 uint16_t adcValTemp = 0;
-float temperature = 0;
+float temperatureAcquisition = 0;
+float temperatureFiltered = 0;
+uint8_t tblFlag = 0;
+float tblData[FILTRE_SIZE];
+uint8_t tblIndex = 0;
 
 // Prototypes des fonctions locales
 /**
@@ -36,6 +42,8 @@ void timer1Init();
  *@brief  Fonction qui regroupe l'initialisation des différents I/O et des librairies.
  */
 void miscInit(void);
+
+float filtreFenetre(float tempRaw);
 
 int main(void)
 {
@@ -50,10 +58,12 @@ int main(void)
 		if (cinqCentMSFlag) // Flag qui est vrai à chaque cinq cent ms.
 		{
 			cinqCentMSFlag = 0;
-			sprintf(msgTemp, "Temperature:%0.1f \n\r", temperature); // Conversion de la mesure de température en string.
+			
+			temperatureFiltered = filtreFenetre(temperatureAcquisition);
+			sprintf(msgTemp, "Temperature: %0.1f  %0.1f\n\r", temperatureAcquisition, temperatureFiltered); // Conversion de la mesure de température en string.
 			usartSendString(msgTemp);
 		}
-		temperature = (float)adcValTemp / 22; // Puisque le capteur de température est un capteur linéaire, 22 à été obtenu en prenant une valeur en début et en la divisant par un nombre qui nous permet d'arriver à la température actuelle.
+		temperatureAcquisition = (float)adcValTemp / 8.903512712; // Puisque le capteur de température est un capteur linéaire, 22 à été obtenu en prenant une valeur en début et en la divisant par un nombre qui nous permet d'arriver à la température actuelle.
 	}
 }
 
@@ -76,6 +86,10 @@ void miscInit(void)
 	timer1Init(); // Initialisation du timers #1.
 	adcInit();	  // Initialisation du adc.
 	usartInit(1000000, F_CPU);
+	for (uint8_t i = 0; i < FILTRE_SIZE; i++)
+	{
+		tblData[i] = 0;
+	}
 }
 
 void timer1Init()
@@ -88,4 +102,28 @@ void timer1Init()
 	TIMSK1 |= (1 << OCIE1A); // Output Compare A Match Interrupt Enable
 	OCR1A = 250 - 1;		 // 62.5ns * 256 * 250 * 125 = 500ms
 	sei();
+}
+
+float filtreFenetre(float tempRaw)
+{
+	float valFiltre = 0;
+tblData[tblIndex++] = tempRaw;
+if (tblIndex >= FILTRE_SIZE)
+{
+	tblFlag = 1;
+	tblIndex = 0;
+}
+if (tblFlag)
+{
+	for (uint8_t i = 0; i < FILTRE_SIZE; i++)
+	{
+		valFiltre += tblData[i];
+	}
+	valFiltre /= FILTRE_SIZE;
+}
+    else
+    {
+        valFiltre = tempRaw;
+    }
+    return valFiltre;
 }
